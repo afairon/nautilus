4b800ad49419f4b19c24bc29d4afa7379a9ca745
BossPokin <pokin67890@gmail.com>
2021-09-16 09:52:11 +0700

    solve dependenciy problems

--- server/grpc.go
+++ server/grpc.go
@@ -1,11 +1,16 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/afairon/nautilus/pb"
 	"github.com/afairon/nautilus/service"
+	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
+
+	"database/sql"
 )
 
 func CreateGRPCServer() *grpc.Server {
@@ -18,10 +23,37 @@ func CreateGRPCServer() *grpc.Server {
 	server := grpc.NewServer(opts...)
 
 	registerServices(server)
+	NewServer()
 
 	return server
 }
 
 func registerServices(server *grpc.Server) {
 	pb.RegisterAccountServer(server, &service.AccountService{})
+}
+
+func NewServer() {
+	const (
+		host     = "nautilus"
+		port     = 5432
+		user     = "boss"
+		password = "IAmNewToLinux1@8259"
+		dbname   = "test"
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=require",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
