0cfde27192a3f882a5a6f5364642a2ffbd121042
BossPokin <pokin67890@gmail.com>
2021-09-16 11:16:26 +0700

    Remove old codes

--- server/grpc.go
+++ server/grpc.go
@@ -1,15 +1,12 @@
 package server
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/afairon/nautilus/pb"
 	"github.com/afairon/nautilus/service"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
-
-	"database/sql"
 )
 
 func CreateGRPCServer() *grpc.Server {
@@ -22,36 +19,10 @@ func CreateGRPCServer() *grpc.Server {
 	server := grpc.NewServer(opts...)
 
 	registerServices(server)
-	NewDB()
 
 	return server
 }
 
 func registerServices(server *grpc.Server) {
 	pb.RegisterAccountServer(server, &service.AccountService{})
-}
-
-func NewDB() {
-	const (
-		host     = "139.59.101.136"
-		port     = 5432
-		user     = "boss"
-		password = "IAmNewToLinux1@8259"
-		dbname   = "test"
-	)
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=require",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println(err)
-	}
 }
