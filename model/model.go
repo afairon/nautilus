67b7f8c43079adb97883504826f054f2ff11ca21
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-02-05 22:35:00 +0700

    define Account class

--- model/model.go
+++ model/model.go
@@ -1,1 +1,26 @@
 package model
+
+import "gorm.io/gorm"
+
+// enum implementation for AccountType
+type AccountType int32
+
+const (
+	ADMIN AccountType = iota
+	AGENCY
+	DIVER
+)
+
+func (a AccountType) String() string {
+	return [...]string{"ADMIN", "AGENCY", "DIVER"}[a]
+}
+
+type Account struct {
+	gorm.Model
+	Email    string
+	Username string
+	Password string
+	Type     AccountType
+	Verified bool
+	Active   bool
+}
