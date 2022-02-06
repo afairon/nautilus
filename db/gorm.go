0d57d176c2ffcb9557357eb241b2fac98f1fc778
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-02-06 08:55:21 +0700

    implement auto migrate

--- db/gorm.go
+++ db/gorm.go
@@ -3,9 +3,12 @@ package db
 import (
 	"fmt"
 
+	"github.com/afairon/nautilus/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+var GormStore *gorm.DB
 
 func NewGormConnection(host string, user string, password string, dbname string, port int, ssl bool) (*gorm.DB, error) {
 	var dsn string
@@ -28,11 +31,16 @@ func NewGormConnection(host string, user string, password string, dbname string,
 		)
 	}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	GormStore, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		return nil, err
 	}
 
-	return db, nil
+	return GormStore, nil
+}
+
+// Automatically migrate when the schemas change
+func MigrateGormStore() {
+	GormStore.AutoMigrate(&model.Account{}, &model.Agency{}, &model.Diver{})
 }
