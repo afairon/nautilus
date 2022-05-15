7b2bd097aa362436ef11c8995276d6e7c1772ced
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-05-15 14:53:43 +0700

    implement AccountRepositorySuite WIP

--- repo/account_test.go
+++ repo/account_test.go
@@ -1,16 +1,49 @@
 package repo_test
 
 import (
+	"database/sql"
+	"testing"
+
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/afairon/nautilus/repo"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type AccountRepositorySuite struct {
 	suite.Suite
-	DB *gorm.DB
-	sqlmock.Sqlmock
+	DB   *gorm.DB
+	mock sqlmock.Sqlmock
 
 	repository repo.AccountRepository
+}
+
+func (s *AccountRepositorySuite) SetupSuite() {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	db, s.mock, err = sqlmock.New()
+	require.NoError(s.T(), err)
+
+	s.DB, err = gorm.Open(postgres.New(postgres.Config{
+		Conn: db,
+	}))
+	require.NoError(s.T(), err)
+
+	s.DB.Logger.LogMode(logger.Info)
+
+	s.repository = repo.NewAccountRepository(s.DB)
+}
+
+func (s *AccountRepositorySuite) AfterTest(_, _ string) {
+	require.NoError(s.T(), s.mock.ExpectationsWereMet())
+}
+
+func TestInit(t *testing.T) {
+	suite.Run(t, new(AccountRepositorySuite))
 }
