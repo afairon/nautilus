c7f9f9dce094f5ca8ad23894d3c23d21942cc50d
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-05-16 20:17:00 +0700

    implement TestAgencyAddDiveMaster

--- handler/agency_test.go
+++ handler/agency_test.go
@@ -1,7 +1,48 @@
 package handler_test
 
-import "testing"
+import (
+	"context"
+	"errors"
+	"testing"
 
-func TestAgency(t *testing.T) {
+	"github.com/afairon/nautilus/handler"
+	"github.com/afairon/nautilus/pb"
+	"github.com/afairon/nautilus/service"
+	"github.com/stretchr/testify/assert"
+)
 
+func TestAgencyAddDiveMaster(t *testing.T) {
+	t.Run("successful", func(t *testing.T) {
+		//Arrange
+		ctx := context.Background()
+		req := &pb.AddDiveMasterRequest{
+			DiveMaster: &pb.DiveMaster{},
+		}
+		agencyService := service.NewAgencyServiceMock()
+		agencyService.On("AddDiveMaster", ctx, req.DiveMaster).Return(nil)
+		agencyHandler := handler.NewAgencyHandler(agencyService)
+
+		//Act
+		_, err := agencyHandler.AddDiveMaster(ctx, req)
+
+		//Assert
+		assert.NoError(t, err)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		//Arrange
+		ctx := context.Background()
+		req := &pb.AddDiveMasterRequest{
+			DiveMaster: &pb.DiveMaster{},
+		}
+		agencyService := service.NewAgencyServiceMock()
+		agencyService.On("AddDiveMaster", ctx, req.DiveMaster).Return(errors.New(""))
+		agencyHandler := handler.NewAgencyHandler(agencyService)
+
+		//Act
+		_, err := agencyHandler.AddDiveMaster(ctx, req)
+
+		//Assert
+		assert.Error(t, err)
+	})
 }
