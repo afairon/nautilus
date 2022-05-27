120d9aa94db19e8160d111786b0530b6a0e7f70e
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-05-27 18:00:37 +0700

    implement TestPaymentMakePayment

--- handler/payment_test.go
+++ handler/payment_test.go
@@ -1,1 +1,57 @@
 package handler_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/afairon/nautilus/handler"
+	"github.com/afairon/nautilus/model"
+	"github.com/afairon/nautilus/pb"
+	"github.com/afairon/nautilus/service"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPaymentMakePayment(t *testing.T) {
+	type paymentMakePaymentTestCase struct {
+		name          string
+		expectedError error
+	}
+
+	ctx := context.Background()
+	req := &pb.MakePaymentRequest{
+		Payment: &pb.Payment{},
+	}
+	modelPayment := model.Payment{}
+	modelPayment.From(req.Payment)
+
+	testCases := []paymentMakePaymentTestCase{
+		{
+			name:          "successful",
+			expectedError: nil,
+		},
+		{
+			name:          "fail",
+			expectedError: errors.New(""),
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			//Arrange
+			paymentService := service.NewPaymentServiceMock()
+			paymentService.On("MakePayment", ctx, &modelPayment).Return(c.expectedError)
+			paymentHandler := handler.NewPaymentHandler(paymentService)
+
+			//Act
+			_, err := paymentHandler.MakePayment(ctx, req)
+
+			//Assert
+			if c.expectedError != nil {
+				assert.ErrorIs(t, err, c.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
