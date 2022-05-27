bec292e1dcc6afc71d399103c9116df1744a2211
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-05-27 18:30:12 +0700

    implement TestPaymentUpdatePaymentSlip

--- handler/payment_test.go
+++ handler/payment_test.go
@@ -55,3 +55,47 @@ func TestPaymentMakePayment(t *testing.T) {
 		})
 	}
 }
+
+func TestPaymentUpdatePaymentSlip(t *testing.T) {
+	type paymentUpdatePaymentSlipTestCase struct {
+		name          string
+		expectedError error
+	}
+
+	ctx := context.Background()
+	req := &pb.UpdatePaymentRequest{
+		Payment: &pb.Payment{},
+	}
+	modelPayment := model.Payment{}
+	modelPayment.From(req.Payment)
+
+	testCases := []paymentUpdatePaymentSlipTestCase{
+		{
+			name:          "successful",
+			expectedError: nil,
+		},
+		{
+			name:          "fail",
+			expectedError: errors.New(""),
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			//Arrange
+			paymentService := service.NewPaymentServiceMock()
+			paymentService.On("UpdatePaymentSlip", ctx, &modelPayment).Return(c.expectedError)
+			paymentHandler := handler.NewPaymentHandler(paymentService)
+
+			//Act
+			_, err := paymentHandler.UpdatePaymentSlip(ctx, req)
+
+			//Assert
+			if c.expectedError != nil {
+				assert.ErrorIs(t, err, c.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
